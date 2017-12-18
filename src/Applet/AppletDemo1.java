6c2968f113ac5bf212953f5d05437dff49a11623
nawarajjaishi <rajanrajjaishi288@gmail.com>
2017-12-18 10:51:52 +0545

    my first commit.

---
+++ src/Applet/AppletDemo1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Applet;
+
+import java.awt.Graphics;
+import java.applet.*;
+
+/**
+ *
+ * @author Jaction76
+ */
+public class AppletDemo1 extends Applet {
+    public void paint(Graphics g){
+     g.drawString("Hello World",150,100);
+    }
+    
+}
