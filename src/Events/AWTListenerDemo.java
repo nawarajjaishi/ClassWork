a4bda181fd0f791837ee39f3557dac19ddcaa711
nawarajjaishi <rajanrajjaishi288@gmail.com>
2017-12-18 11:38:31 +0545

    this is Events handling package

---
+++ src/Events/AWTListenerDemo.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Events;
+
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author Jaction76
+ */
+public class AWTListenerDemo {
+   private Frame mainFrame;
+   private Label headerLabel;
+   private Label statusLabel;
+   private Panel controlPanel;
+   private TextField textField;
+   
+   public AWTListenerDemo(){
+      prepareGUI();
+   }
+
+   public static void main(String[] args){
+      AWTListenerDemo awtListenerDemo = new AWTListenerDemo();  
+      awtListenerDemo.showKeyListenerDemo();
+   }
+
+   private void prepareGUI(){
+      mainFrame = new Frame("Java AWT Examples");
+      mainFrame.setSize(400,400);
+      mainFrame.setLayout(new GridLayout(3, 1));
+      mainFrame.addWindowListener(new WindowAdapter() {
+         public void windowClosing(WindowEvent windowEvent){
+            System.exit(0);
+         }        
+      });    
+   
+      headerLabel = new Label();
+      headerLabel.setAlignment(Label.CENTER);
+      statusLabel = new Label();        
+      statusLabel.setAlignment(Label.CENTER);
+      statusLabel.setSize(350,100);
+
+      controlPanel = new Panel();
+      controlPanel.setLayout(new FlowLayout());
+
+      mainFrame.add(headerLabel);
+      mainFrame.add(controlPanel);
+      mainFrame.add(statusLabel);
+      mainFrame.setVisible(true);  
+   }
+
+   private void showKeyListenerDemo(){
+      headerLabel.setText("Listener in action: KeyListener");      
+
+      textField  = new TextField(10);
+
+      textField.addKeyListener(new CustomKeyListener());
+      Button okButton = new Button("OK");
+      okButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            statusLabel.setText("Entered text: " + textField.getText());                
+         }
+      });
+
+      controlPanel.add(textField);
+      controlPanel.add(okButton);    
+      mainFrame.setVisible(true);  
+   }
+
+   class CustomKeyListener implements KeyListener{
+      public void keyTyped(KeyEvent e) {           
+      }
+
+      public void keyPressed(KeyEvent e) {
+         if(e.getKeyCode() == KeyEvent.VK_ENTER){
+            statusLabel.setText("Entered text: " + textField.getText());
+         }
+      }
+
+      public void keyReleased(KeyEvent e) {            
+      }    
+   } 
+    
+}
